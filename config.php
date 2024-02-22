cf4595b5dd6840b5b86775cccf29f8ddc9594f66
tarmo66 <tarmo@traktorist.org>
2024-02-22 11:28:28 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = 'localhost';
-$db_username = 'r293685_actyUser';
-$db_password = 'KK4#pxD15(QV';
-$db_name = 'r293685_acty';
+$db_username = 'user';
+$db_password = 'password';
+$db_name = 'database';
 $conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
